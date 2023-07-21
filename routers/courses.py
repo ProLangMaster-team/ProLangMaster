8d4e0223ebc109850319b2df2c8de7329c938405
Pratik Choudhari <choudh12@uwindsor.ca>
2023-07-21 12:56:22 -0400

    [UPDATE] add course exist check in enroll course api

--- routers/courses.py
+++ routers/courses.py
@@ -1,9 +1,9 @@
 import bson
 from fastapi import APIRouter
 
-from models.pydantic_models import CourseDetails, JWTToken, AuthCourseID
+from models.pydantic_models import CourseDetails, JWTToken, AuthCourseID, EnrollCourse
 from utils.api_utils import verify_jwt
-from utils.database import users_db, courses_db, pymongo_exceptions
+from utils.database import users_db, courses_db
 
 router = APIRouter()
 
@@ -30,11 +30,15 @@ async def get_flashcards(data: AuthCourseID):
 
 
 @router.put("/enroll")
-async def enroll_course(data: AuthCourseID):
+async def enroll_course(data: EnrollCourse):
     if not verify_jwt(data.token):
         return {"status": "error", "message": "unauthenticated user"}
 
-    tmp = courses_db.find_one({"_id": bson.ObjectId(data.course_id)})
-    if tmp:
-        return {'status': 'ok', 'message': 'success', "data": tmp["quiz"]}
-    return {'status': 'error', 'message': 'Course does not exist'}
+    count = courses_db.count_documents({"_id": bson.ObjectId(data.course_id)})
+    if count == 0:
+        return {'status': 'error', 'message': 'Course does not exist'}
+
+    users_db.update_one({"email": data.email}, {"$push": {"enrolled_courses": {"course_id": data.course_id,
+                                                                               "fluency": data.fluency,
+                                                                               "frequency": data.frequency}}})
+    return {'status': 'ok', 'message': 'success'}
