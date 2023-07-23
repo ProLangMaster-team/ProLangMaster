b92f677fa069c0d88d3ccb0e88f0b9b8829a919e
Haard-Mehta <mehta1e@uwindsor.ca>
2023-07-23 12:33:09 -0400

    correct incorrect pages modified

--- pro_lang_master/lib/Login/incorrect.dart
+++ pro_lang_master/lib/Login/incorrect.dart
@@ -66,47 +66,4 @@ class incorrect extends State<InCorrect> {
     // TODO: implement createState
     throw UnimplementedError();
   }
-
-  String? ValidateEmail(String? value) {
-    const pattern = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'"
-        r'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-'
-        r'\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*'
-        r'[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4]'
-        r'[0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9]'
-        r'[0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\'
-        r'x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])';
-    final regex = RegExp(pattern);
-
-    return value!.isNotEmpty && !regex.hasMatch(value)
-        ? 'Enter a valid email address'
-        : null;
-  }
-
-  void forgotPassword() async {
-    print(emailController.text);
-    var requestBody = {
-      "email": emailController.text,
-    };
-    final headers = {HttpHeaders.contentTypeHeader: 'application/json'};
-    Uri uri = Uri.https("https://e2c7-137-207-232-218.ngrok-free.app",
-        "/reset-password/exist", requestBody);
-    print(uri);
-    var response = await http.get(uri);
-    print(response.statusCode);
-    print(json.decode(response.body));
-    if (json.decode(response.body)['status'] == 'error') {
-      print('Error');
-      setState(() {
-        errorCase = true;
-      });
-    } else {
-      setState(() {
-        errorCase = false;
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => Verification()),
-        );
-      });
-    }
-  }
 }
